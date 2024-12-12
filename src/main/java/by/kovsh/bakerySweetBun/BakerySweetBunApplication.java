08e078ae0e1cb3e640e0b4c3f24659aa63269645
Andrej <abcccde@mail.ru>
2024-12-12 18:45:50 +0300

    My project bakerySweetBun

---
+++ src/main/java/by/kovsh/bakerySweetBun/BakerySweetBunApplication.java
@@ -0,0 +1,13 @@
+package by.kovsh.bakerySweetBun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakerySweetBunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakerySweetBunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/by/kovsh/bakerySweetBun/BakerySweetBunApplicationTests.java
@@ -0,0 +1,13 @@
+package by.kovsh.bakerySweetBun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakerySweetBunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
